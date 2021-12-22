da72f329e40750609896042dc4aab9a27f393027
lukemjohnston <72853815+lukemjohnston@users.noreply.github.com>
2021-12-22 00:44:58 -0800

    random math formula generator

--- app/src/main/java/com/example/mathflash/game.java
+++ app/src/main/java/com/example/mathflash/game.java
@@ -5,13 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class game extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int var = (int)Math.floor(Math.random()*(3-1+1)+1);
+        int min = 1;
+        int max = 2;
+        int ans = 0;
+
+        if(var < 3) {
+            min = 1;
+            max = 50;
+        }
+        if(var == 3) {
+            min = 1;
+            max = 12;
+        }
+
+        int number1 = (int)Math.floor(Math.random()*(max-min+1)+min);
+        int number2 = (int)Math.floor(Math.random()*(max-min+1)+min);
+        String n1 = String.valueOf(number1);
+        String n2 = String.valueOf(number2);
+        String v = "$";
+
+        if(var == 1) {      //addition
+            ans = number1 + number2;
+            v = "+";
+        }
+        if(var == 2) {      //subtraction
+            ans = number1 - number2;
+            v = "-";
+        }
+        if(var == 3) {      //multiplication
+            ans = number1 * number2;
+            v = "x";
+        }
+        String formula = n1+" "+v+" "+n2+" =  ?";
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        TextView textView = (TextView) findViewById(R.id.mathFormula);
+        textView.setText(formula);
     }
 
     public void optionChosen(View view) {
