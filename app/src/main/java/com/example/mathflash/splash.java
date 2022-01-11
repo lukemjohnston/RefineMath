25495f3b99a04ed6abad3fe982094f4cbe120338
lukemjohnston <72853815+lukemjohnston@users.noreply.github.com>
2022-01-11 12:57:05 -0800

    splash screen

---
+++ app/src/main/java/com/example/mathflash/splash.java
@@ -0,0 +1,26 @@
+package com.example.mathflash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+        }, 5000);
+    }
+}
